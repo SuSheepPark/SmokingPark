e5d4fae0a5bd65ec6f72199162a06f2343e0e7d1
zhao wei <1540476506@qq.com>
2019-09-07 19:18:40 +0800

    "提交到本地缓存"

---
+++ src/main/java/com/zhaowei/common/utils/WeekTest.java
@@ -0,0 +1,61 @@
+/**   
+ * Copyright © 2019 公司名. All rights reserved.
+ * 
+ * @Title: WeekTest.java 
+ * @Prject: zhaowei.common
+ * @Package: com.zhaowei.common.utils 
+ * @Description: TODO
+ * @author: zw   
+ * @date: 2019年9月7日 下午4:31:04 
+ * @version: V1.0   
+ */
+package com.zhaowei.common.utils;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/** 
+ * @ClassName: WeekTest 
+ * @Description: TODO
+ * @author: zw
+ * @date: 2019年9月7日 下午4:31:04  
+ */
+public class WeekTest {
+
+	/**
+	 * 
+	 * @Title: toHtml   
+	 * @Description: 传来的字符转成 html 文本，遇到“\n”符时，要用<p></p>将这一段
+		字符包起来。 
+	 * @return
+	 * @return: String
+	 */	
+	
+	public static String toHtml(String src) {
+		String str = src.replaceAll(System.getProperty("line.separator"), "|")	;
+		String[] split = str.split("\\|");
+		String newStr="";
+		for (String string2 : split) {
+			newStr+="<p>"+string2+"</p>";
+		}
+		return newStr;
+	}
+	
+	
+	//手机号验证
+	public static boolean checkCellphone(String str) {
+		String refgExp = "^[1](39|137|178)[0-9]{8}$";
+		Pattern p = Pattern.compile(refgExp);
+		Matcher m = p.matcher(str);
+		return m.matches();
+	    }
+	//邮箱验证
+	public static boolean checkCellemail(String str) {
+		String refgExp = "^\\w+@[a-z0-9]+\\.[a-z]{2,4}$";
+		Pattern p = Pattern.compile(refgExp);
+		Matcher m = p.matcher(str);
+		return m.matches();
+		}
+}
+	
+
---
+++ src/test/java/com/zhaowei/common/utils/WeekTestTest.java
@@ -0,0 +1,44 @@
+/**   
+ * Copyright © 2019 公司名. All rights reserved.
+ * 
+ * @Title: WeekTestTest.java 
+ * @Prject: zhaowei.common
+ * @Package: com.zhaowei.common.utils 
+ * @Description: TODO
+ * @author: zw   
+ * @date: 2019年9月7日 下午4:59:36 
+ * @version: V1.0   
+ */
+package com.zhaowei.common.utils;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/** 
+ * @ClassName: WeekTestTest 
+ * @Description: TODO
+ * @author: zw
+ * @date: 2019年9月7日 下午4:59:36  
+ */
+public class WeekTestTest {
+
+	/**
+	 * Test method for {@link com.zhaowei.common.utils.WeekTest#checkCellphone(java.lang.String)}.
+	 */
+	@Test
+	public void testCheckCellphone() {
+		boolean checkCellphone = WeekTest.checkCellphone("13912345678");
+		System.out.println(checkCellphone);
+	}
+	@Test
+	public void testcheckCellemail() {
+		boolean checkCellemail = WeekTest.checkCellemail("1540476506@qq.com");
+		if(checkCellemail) {
+			System.out.println("验证成功");
+		}else {
+			System.out.println("验证失败");
+		}
+	}
+	
+}
